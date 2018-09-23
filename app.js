b8c4c2fad4b2f71f93840c230156449bee677a01
Pavel Bragin <bragin_pa@soshace.com>
2018-09-23 23:24:40 +0300

    Passport JWT

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ var promoRouter = require('./routes/promoRouter');
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -33,35 +36,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-    name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-}));
-
-function auth (req, res, next) {
-    console.log(req.session);
-
-    if(!req.session.user) {
-        var err = new Error('You are not authenticated!');
-        err.status = 403;
-        return next(err);
-    }
-    else {
-        if (req.session.user === 'authenticated') {
-            next();
-        }
-        else {
-            var err = new Error('You are not authenticated!');
-            err.status = 403;
-            return next(err);
-        }
-    }
-}
-
-app.use(auth);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
